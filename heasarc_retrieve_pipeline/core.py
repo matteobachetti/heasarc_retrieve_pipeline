9c4031d35bc8af76e057828ab553349345bc5d8c
Matteo Bachetti <matteo@matteobachetti.it>
2025-05-28 15:28:26 +0200

    Adjust test position

--- heasarc_retrieve_pipeline/core.py
+++ heasarc_retrieve_pipeline/core.py
@@ -273,31 +273,3 @@ def retrieve_heasarc_data_by_source_name(
         )
 
     return results
-
-
-@pytest.mark.parametrize(
-    "mission",
-    ["nustar", "nicer"],
-)
-def test_retrieve_heasarc_data_by_source_name(mission):
-    results = retrieve_heasarc_data_by_source_name(
-        "M82 X-2", mission=mission, test=True
-    )
-    assert len(results) > 0
-
-
-def test_recursive_download():
-    import shutil
-    import re
-
-    results = recursive_download(
-        "https://heasarc.gsfc.nasa.gov/FTP/nustar/data/obs/00/8/80002092003/",
-        "out_test",
-        cut_ndirs=0,
-        test_str=".",
-        test=False,
-        re_include=r"[AB]0.*evt",
-        re_exclude=r"[AB]0[2-5]",
-    )
-    assert len(results) == 2
-    shutil.rmtree("out_test")
--- heasarc_retrieve_pipeline/tests/test_example.py
+++ heasarc_retrieve_pipeline/tests/test_example.py
@@ -1,13 +1,30 @@
+import pytest
+from heasarc_retrieve_pipeline.core import retrieve_heasarc_data_by_source_name, recursive_download
 
 
-def test_primes():
-    from ..example_mod import primes
-    assert primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+@pytest.mark.remote_data
+@pytest.mark.parametrize(
+    "mission",
+    ["nustar", "nicer"],
+)
+def test_retrieve_heasarc_data_by_source_name(mission):
+    results = retrieve_heasarc_data_by_source_name("M82 X-2", mission=mission, test=True)
+    assert len(results) > 0
 
 
-def test_deprecation():
-    import warnings
-    warnings.warn(
-        "This is deprecated, but shouldn't raise an exception, unless "
-        "enable_deprecations_as_exceptions() called from conftest.py",
-        DeprecationWarning)
+@pytest.mark.remote_data
+def test_recursive_download():
+    import shutil
+    import re
+
+    results = recursive_download(
+        "https://heasarc.gsfc.nasa.gov/FTP/nustar/data/obs/00/8/80002092003/",
+        "out_test",
+        cut_ndirs=0,
+        test_str=".",
+        test=False,
+        re_include=r"[AB]0.*evt",
+        re_exclude=r"[AB]0[2-5]",
+    )
+    assert len(results) == 2
+    shutil.rmtree("out_test")
