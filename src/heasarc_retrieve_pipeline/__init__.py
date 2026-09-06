7163ff2c71b81bd5b6f3f9f22974ef4ec0d00b16
Matteo Bachetti <matteo@matteobachetti.it>
2026-09-06 06:25:06 +0200

    Log the package version at info level on import

--- src/heasarc_retrieve_pipeline/__init__.py
+++ src/heasarc_retrieve_pipeline/__init__.py
@@ -1,4 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import logging
+
 from ._version import version as __version__
 
 __all__ = ["__version__"]
+
+logging.getLogger(__name__).info("heasarc_retrieve_pipeline version %s", __version__)
